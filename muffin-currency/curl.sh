ea43251738a74a16a954e775c9860839e2eaf260
germanal2k4 <qwestard@gmail.com>
2025-12-15 14:55:10 +0000

    fixed metrics

--- muffin-currency/curl.sh
+++
@@ -1,8 +0,0 @@
-curl -X 'POST' \
-  'http://localhost:8080/v1/muffin-wallet/f1691d7e-9ae7-4195-99ab-2c46fbbf78ad/transaction' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "to_muffin_wallet_id": "682ecb9b-0331-444a-8e6d-19cd7aeab737",
-  "amount": 1
-}'
